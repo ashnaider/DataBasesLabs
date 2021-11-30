6a42852b7acc89469d116a32f7b580de50edbd80
ashnaider <ash2003@ukr.net>
2021-11-30 22:33:41 +0200

    added notes for task 7, added rows for tables

---
+++ lab3/07_task.sql
@@ -0,0 +1,15 @@
+-- 7.  Получить информацию об объявлениях, для которых дедлайн, установленный заказчиком,
+-- наступает позже, чем дедлайн, установленный фрилансером.
+
+
+CURR_DATE constant date := '2023-01-03';
+
+
+select app.id, nj.deadline as customer_deadline,
+       app.deadline as freelancer_deadline,
+       nj.price as customer_price,
+       app.price as freelancer_price,
+       app.description
+from application as app
+left join new_job as nj on nj.id = app.job_id
+where app.deadline < nj.deadline;
--- lab3/all_solutions.sql
+++ lab3/all_solutions.sql
@@ -23,6 +23,20 @@ select  text_message,
 
 
 
+-- 7.  Получить информацию об объявлениях, для которых дедлайн, установленный заказчиком,
+-- наступает позже, чем дедлайн, установленный фрилансером.
+
+
+select app.id, nj.deadline as customer_deadline,
+       app.deadline as freelancer_deadline,
+       nj.price as customer_price,
+       app.price as freelancer_price,
+       app.description
+from application as app
+left join new_job as nj on nj.id = app.job_id
+where app.deadline < nj.deadline;
+
+
 
 
 -- 9. Получить список фрилансеров, имеющих одинаковые специализации,
